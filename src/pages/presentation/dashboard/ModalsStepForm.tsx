69fab6970d4ebe05b020b70b2dfd02be2b2f991d
pawan <pawansinghania@creativebuffer.com>
2023-06-22 13:59:37 +0530

    step form ui design

--- src/pages/presentation/dashboard/ModalsStepForm.tsx
+++ src/pages/presentation/dashboard/ModalsStepForm.tsx
@@ -1,146 +1,385 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
-import Page from '../../../layout/Page/Page';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Field, useFormik } from "formik";
+import Page from "../../../layout/Page/Page";
 import Modal, {
-	ModalBody,
-	ModalFooter,
-} from '../../../components/bootstrap/Modal';
-import Input from '../../../components/bootstrap/forms/Input';
-import FormGroup from '../../../components/bootstrap/forms/FormGroup';
-
-
-
+  ModalBody,
+  ModalFooter,
+} from "../../../components/bootstrap/Modal";
+import Input from "../../../components/bootstrap/forms/Input";
+import FormGroup from "../../../components/bootstrap/forms/FormGroup";
+import Checks from "../../../components/bootstrap/forms/Checks";
+import Select from "../../../components/bootstrap/forms/Select";
 
 const ModalsStepForm: React.FC = () => {
-	const savedValue = localStorage.getItem('user');
-	const parsedValue = savedValue ? JSON.parse(savedValue) : null;
-	// Access the properties of the parsed object
-	const newUserEmail = parsedValue?.newUserEmail;
-	const newUserName = parsedValue?.newUserName;
-	const newUserPassword = parsedValue?.newUserPassword;
-	const newUserSurname = parsedValue?.newUserSurname;
-// Formik uses
-const formik = useFormik({
-	enableReinitialize: true,
-	initialValues: {
-		newUserEmail: '',
-		newUserteliphone: '',
-		newUserConformation: '',
-		newUserPassword: '',
-	},
-	validate: (values) => {
-		const errors: {
-			newUserEmail?: string;
-			newUserteliphone?: string;
-			newUserConformation?: string;
-			newUserPassword?: string;
-		} = {};
-		if (!values.newUserEmail) {
-			errors.newUserEmail = 'Required';
-		}
+  const savedValue = localStorage.getItem("user");
+  const parsedValue = savedValue ? JSON.parse(savedValue) : null;
+  // Access the properties of the parsed object
+  const newUserEmail = parsedValue?.newUserEmail;
+  const newUserName = parsedValue?.newUserName;
+  const newUserPassword = parsedValue?.newUserPassword;
+  const newUserSurname = parsedValue?.newUserSurname;
+  // Formik uses
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      newUserEmail: "",
+      newUserteliphone: "",
+      newUserPassword: "",
+      newUserConformation: "",
+    },
+    validate: (values) => {
+      const errors: {
+        newUserEmail?: string;
+        newUserteliphone?: string;
+        newUserPassword?: string;
+        newUserConformation?: string;
+      } = {};
+      if (!values.newUserEmail) {
+        errors.newUserEmail = "Required";
+      }
 
-		if (!values.newUserteliphone) {
-			errors.newUserteliphone = 'Required';
-		}
-		if (!values.newUserConformation) {
-			errors.newUserConformation = 'Required';
-		}
-		if (!values.newUserPassword) {
-			errors.newUserPassword = 'Required';
-		}
+      if (!values.newUserteliphone) {
+        errors.newUserteliphone = "Required";
+      }
+      if (!values.newUserPassword) {
+        errors.newUserPassword = "Required";
+      }
+      if (!values.newUserConformation) {
+        errors.newUserConformation = "Required";
+      }
 
-		return errors;
-	},
-	validateOnChange: false,
-	onSubmit: (_values) => {},
-});
+      return errors;
+    },
+    validateOnChange: false,
+    onSubmit: (_values) => {},
+  });
 
-// Steps forms
-	const [isOpen, setIsOpen] = useState(true);
-	const [isOpenModal, setIsOpenModal] = useState(true);
-	const steps = ['Step 1', 'Step 2', 'Step 3','Step 4','Step 5','Step 6','Step 7',];
-	const [currentStep, setCurrentStep] = useState(0);
-	const navigate = useNavigate();
+  // Steps forms
+  const [isOpen, setIsOpen] = useState(true);
+  const [isOpenModal, setIsOpenModal] = useState(true);
+  const steps = [
+    "Step 1",
+    "Step 2",
+    "Step 3",
+    "Step 4",
+    "Step 5",
+    "Step 6",
+    "Step 7",
+  ];
+  const [currentStep, setCurrentStep] = useState(0);
+  const navigate = useNavigate();
 
-	const nextStep = () => {
-		setCurrentStep(currentStep + 1);
-	  };
-	
-	  const previousStep = () => {
-		setCurrentStep(currentStep - 1);
-	  };
-	  const closeModal = () => {
-		setIsOpenModal(false);
-		navigate('/')
-	  };
-	
+  const nextStep = () => {
+    setCurrentStep(currentStep + 1);
+  };
 
-	return (
-		<Page className='w-100 mt-10'>
-			<Modal isOpen setIsOpen={setIsOpen} id='sdmsk12' className='modal content: {width: "400px",height: "300px",margin: "auto"}'>
-				<ModalBody>
-					<div className='text-center h1  fw-bold'>MA OSSIM</div>
-					{(steps[currentStep]==='Step 1')?(<div>
-						<div className='text-center h1  fw-bold'>Hi {newUserName}</div>
-						<div className='col-12'>
-							<FormGroup id='signup-email' isFloating label='EMAIL'>
-								<Input type='email' name='newUserName' value={newUserEmail} />
-							</FormGroup>
-						</div>
-						<div className='col-12 mt-2'>
-							
-								<Input
-									type='tel'
-									name='newUserteliphone'
-									value={formik.values.newUserteliphone}
-								/> <p>I agree to the </p>
-							
-						</div>
-						<div className='col-12 mt-2'>
-							<FormGroup id='signup-email' isFloating label='TELEPHONE'>
-								<input
-									type='checkbox'
-									name='newUserConformation'
-									value={formik.values.newUserConformation}
-								/>
-							</FormGroup>
-						</div>
-	
-						<div className='text-center h1 mb-5'>START YOUR FREE TRAIL</div>
-						<div className='text-center h1 mb-5'>
-							Try all the features -{' '}
-							<span className='text-danger'>No credit card required??</span>
-						</div>
-					</div>):(steps[currentStep]==='Step 2')?(<div>{steps[currentStep]}</div>):(steps[currentStep]==='Step 3')?(<div>{steps[currentStep]}</div>):(steps[currentStep]==='Step 4')?(<div>{steps[currentStep]}</div>):(steps[currentStep]==='Step 5')?(<div>{steps[currentStep]}</div>):(steps[currentStep]==='Step 6')?(<div>{steps[currentStep]}</div>):(<div>{steps[currentStep]}</div>)}
-					
-					{/* <div>
-			<p>Email: {newUserEmail}</p>
-			<p>Name: {newUserName}</p>
-			<p>Password: {newUserPassword}</p>
-			<p>Surname: {newUserSurname}</p>
-		</div> */}
-				</ModalBody>
-				<ModalFooter>
-				<h2>{steps[currentStep]}</h2>
-        <p>Step {currentStep + 1} content goes here.</p>
+  const previousStep = () => {
+    setCurrentStep(currentStep - 1);
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+    navigate("/");
+  };
 
-        <div>
-          {currentStep > 0 && (
-            <button onClick={previousStep}>Previous</button>
-          )}
-          {currentStep < steps.length - 1 && (
-            <button onClick={nextStep}>Next</button>
-          )}
-          {currentStep === steps.length - 1 && (
-            <button onClick={closeModal}>Submit</button>
-          )}
-        </div>
+  return (
+    <Page className="w-100 mt-10">
+      <Modal isOpen setIsOpen={setIsOpen} id="sdmsk12" size="lg">
+        <ModalBody>
+          <div className="row">
+            <div className="col-12 col-lg-8 offset-lg-2">
+              {steps[currentStep] === "Step 1" ? (
+                <div>
+                  <div className="text-center h2 my-3 fw-bold">MA OSSIM</div>
+                  <div className="text-center display-2 fw-bold mb-5 lh-1">
+                    START YOUR FREE TRAIL
+                  </div>
+                  <div className="text-center text-muted h5 mb-5">
+                    Try all the features -{" "}
+                    <span className="text-danger">
+                      No credit card required??
+                    </span>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup id="signup-email" isFloating label="EMAIL">
+                      <Input
+                        type="email"
+                        name="newUserName"
+                        value={newUserEmail}
+                      />
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup id="signup-email" isFloating label="TELEPHONE">
+                      <Input
+                        type="tel"
+                        name="newUserteliphone"
+                        onChange={formik.handleChange}
+                        value={formik.values.newUserteliphone}
+                        isValid={formik.isValid}
+                        isTouched={formik.touched.newUserteliphone}
+                        invalidFeedback={formik.errors.newUserteliphone}
+                        onBlur={formik.handleBlur}
+                        onFocus={() => {
+                          formik.setErrors({});
+                        }}
+                      />
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup id="password" isFloating label="PASSWORD">
+                      <Input
+                        type="password"
+                        name="newUserPassword"
+                        onChange={formik.handleChange}
+                        value={formik.values.newUserPassword}
+                        isValid={formik.isValid}
+                        isTouched={formik.touched.newUserPassword}
+                        invalidFeedback={formik.errors.newUserPassword}
+                        onBlur={formik.handleBlur}
+                        onFocus={() => {
+                          formik.setErrors({});
+                        }}
+                      />
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup
+                      id="confirmpassword"
+                      isFloating
+                      label="CONFIRM PASSWORD"
+                    >
+                      <Input
+                        type="password"
+                        name="newUserConformation"
+                        onChange={formik.handleChange}
+                        value={formik.values.newUserConformation}
+                        isValid={formik.isValid}
+                        isTouched={formik.touched.newUserConformation}
+                        invalidFeedback={formik.errors.newUserConformation}
+                        onBlur={formik.handleBlur}
+                        onFocus={() => {
+                          formik.setErrors({});
+                        }}
+                      />
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup>
+                      {/* <input
+                        type="checkbox"
+                        name="newUserConformation"
+                        value={formik.values.newUserConformation}
+                      /> */}
+                      <label className="d-flex">
+                        <Checks type="checkbox" checked="checked"></Checks>I
+                        agree to the
+                        <Link to="" className="mx-2">
+                          Terms of Use
+                        </Link>
+                        and
+                        <Link to="" className="mx-2">
+                          Privacy Policy
+                        </Link>
+                      </label>
+                    </FormGroup>
+                  </div>
+                </div>
+              ) : steps[currentStep] === "Step 2" ? (
+                <div>
+                  <div className="text-center text-dark h2 fw-bold mb-4">
+                    Welcome to MA OSSIM!
+                  </div>
+                  <div className="text-center text-dark h3 mb-4">
+                    Let’s set up your account
+                  </div>
+                  <div className="text-center text-muted h5 mb-4">
+                    You can always change it later
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup id="signup-company" isFloating label="COMPANY">
+                      <Input type="text" name="newUserCompany" value="" />
+                    </FormGroup>
+                  </div>
+                </div>
+              ) : steps[currentStep] === "Step 3" ? (
+                <div>
+                  <div className="text-center text-dark h2 fw-bold mb-4">
+                    Welcome to MA OSSIM!
+                  </div>
+                  <div className="text-center text-dark h4 mb-4">
+                    Let’s set up your account
+                  </div>
+                  <div className="text-center text-muted h5 mb-4">
+                    You can always change it later
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup id="firstname" isFloating label="NAME">
+                      <Input type="text" name="firstname" value="" />
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup id="lastname" isFloating label="SURNAME">
+                      <Input type="text" name="lastname" value="" />
+                    </FormGroup>
+                  </div>
+                </div>
+              ) : steps[currentStep] === "Step 4" ? (
+                <div>
+                  <div className="text-center text-dark h2 fw-bold mb-4">
+                    Which best describes you?
+                  </div>
+                  <div className="text-center text-dark h4 mb-5">
+                    This will help us adapt the platform to fit your business
+                    needs
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup>
+                      <Checks
+                        type="radio"
+                        name="describescheck"
+                        checked="checked"
+                        label="Student"
+                      ></Checks>
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup>
+                      <Checks
+                        type="radio"
+                        label="New Entrepreneur (1 year and less)"
+                        name="describescheck"
+                      ></Checks>
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup>
+                      <Checks
+                        type="radio"
+                        label="Entrepreneur (2-3 years)"
+                        name="describescheck"
+                      ></Checks>
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3">
+                    <FormGroup>
+                      <Checks
+                        type="radio"
+                        label="Entrepreneur (more than 3 years)"
+                        name="describescheck"
+                      ></Checks>
+                    </FormGroup>
+                  </div>
+                  <div className="mb-3 mt-4">
+                    <FormGroup
+                      id="activity"
+                      label="Field of activity"
+                      //   isFloating
+                    >
+                      <Select
+                        ariaLabel="activity"
+                        placeholder="Choose..."
+                        list={[
+                          { value: "a1", text: "Field of activity 1" },
+                          { value: "a2", text: "Field of activity 2" },
+                          { value: "a3", text: "Field of activity 3" },
+                          { value: "a4", text: "Field of activity 4" },
+                        ]}
+                        // onChange={formikAddress.handleChange}
+                        // onBlur={formikAddress.handleBlur}
+                        // value={formikAddress.values.state}
+                        // isValid={formikAddress.isValid}
+                        // isTouched={formikAddress.touched.state}
+                        // invalidFeedback={formikAddress.errors.state}
+                      />
+                    </FormGroup>
+                  </div>
+                </div>
+              ) : steps[currentStep] === "Step 5" ? (
+                <div>
+                  <div className="text-center text-dark h2 fw-bold mb-4">
+                    MA OSSIM
+                  </div>
+                  <div className="text-center text-dark h4 mb-5">
+                    How many are you on your team?
+                  </div>
+                  <div className="team-button text-center">
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark px-4 m-1"
+                    >
+                      Just Me
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark px-4 m-1"
+                    >
+                      2-4
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark px-4 m-1"
+                    >
+                      5-10
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark px-4 m-1"
+                    >
+                      11-30
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark px-4 m-1"
+                    >
+                      31+
+                    </button>
+                  </div>
+                </div>
+              ) : steps[currentStep] === "Step 6" ? (
+                <div>{steps[currentStep]}</div>
+              ) : (
+                <div>{steps[currentStep]}</div>
+              )}
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter className="justify-content-center">
+          {/* <h2>{steps[currentStep]}</h2>
+          <p>Step {currentStep + 1} content goes here.</p> */}
 
-				</ModalFooter>
-			</Modal>
-		</Page>
-	);
+          <div className="steps-action">
+            {currentStep > 0 && (
+              <button onClick={previousStep} className="btn btn-dark px-4 mx-1">
+                Previous
+              </button>
+            )}
+            {currentStep < steps.length - 1 && (
+              <button onClick={nextStep} className="btn btn-primary px-4 mx-1">
+                Next
+              </button>
+            )}
+            {currentStep === steps.length - 1 && (
+              <button
+                onClick={closeModal}
+                className="btn btn-success px-4 mx-1"
+              >
+                Submit
+              </button>
+            )}
+          </div>
+        </ModalFooter>
+        <ModalFooter className="justify-content-center">
+          <div className="mb-3 text-muted">
+            Already have an account?
+            <Link to="" className="ms-2">
+              Login
+            </Link>
+          </div>
+        </ModalFooter>
+      </Modal>
+    </Page>
+  );
 };
 
 export default ModalsStepForm;
